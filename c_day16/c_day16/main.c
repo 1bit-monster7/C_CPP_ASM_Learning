4f0b454c4f550f18fedb1de0a316f572e3aab41f
1bit <1748244285@qq.com>
2023-07-06 12:05:19 +0800

    feat：学习数据宽度

---
+++ c_day16/c_day16/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+	// 一个十六进制数 == 4个二进制数  即   0x0 = 0000 | 0xF  = 1111
+
+	// char			1Byte 8Bit  0000 0000									HEX：0x00 ~ 0xFF
+	// short		2Byte 16Bit 0000 0000 0000 0000							HEX：0x0000 ~ 0xFFFF
+	// int			4Byte 32Bit 0000 0000 0000 0000 0000 0000 0000 0000		HEX：0x 00000000 ~ 0x FFFFFFFF
+	// long			4Byte 32Bit 0000 0000 0000 0000 0000 0000 0000 0000     HEX：0x 00000000 ~ 0x FFFFFFFF
+	// long long    ....
+
+
+	// 0 0 0 0 0 0 0 0 
+	// 1 1 1 1 1 1 1 1 
+	char str = 0;
+
+
+
+	return 0;
+}
