ab1365f076510913de852b021d6e661a97ccd8bc
张文彬 <1748244285@qq.com>
2023-07-18 18:15:50 +0800

    fix：学习内存存储端序

--- c_day19/c_day19/main.c
+++ c_day19/c_day19/main.c
@@ -4,13 +4,13 @@ int main() {
 
 
 
-
-
 	// 强行访问一个未使用的0x12345678 地址 会报错
-	__asm {
-		MOV DWORD PTR DS:[0x12345678],0x66666666
+	//__asm {
+	//	MOV DWORD PTR DS:[0x12345678],0x66666666
 
-		MOV EAX,DWORD PTR DS:[0x12345678]  // 把0x12345678地址的 立即数赋值到EAX中
-	}
+	//	MOV EAX,DWORD PTR DS:[0x12345678]  // 把0x12345678地址的 立即数赋值到EAX中
+	//}	
+
+	int temp = 0x1A2B3C4D; // 从低位到高位
 	return 0;
 }
